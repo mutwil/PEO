40d5b0ea262d791a38bf55bf373489b7443a2bb9
William Goh <wirriamm@gmail.com>
2023-08-28 23:02:49 +0800

    feat: global provider to store navigationPayload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app'
-import React from 'react';
+import { createContext, useEffect, useState } from 'react';
 import 'regenerator-runtime/runtime'
 
 import NextNProgress from 'nextjs-progressbar'
@@ -8,17 +8,24 @@ import '../styles/globals.css'
 // Fontawesome setup for Next.js
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { GeneUnit } from './api/heatmap';
 config.autoAddCss = false
 
+export const GlobalStoreContext = createContext({});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  React.useEffect(() => {
+  useEffect(() => {
     import("flowbite/dist/flowbite.js")
   }, [])
 
-  return <>
-    <NextNProgress height={5} color="#D87F61" />
-    <Component {...pageProps} />
-  </>
+  const [ navigationPayload, setNavigationPayload ] = useState<GeneUnit[]>();
+
+  return (
+    <GlobalStoreContext.Provider value={{ navigationPayload, setNavigationPayload }}>
+      <NextNProgress height={5} color="#D87F61" />
+      <Component {...pageProps} />
+    </GlobalStoreContext.Provider>
+  )
 }
 
 export default MyApp
