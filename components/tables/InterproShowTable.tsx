3423c438a3c235b051bf8ea2dba756124471c7a4
William Goh <wirriamm@gmail.com>
2023-08-28 23:38:51 +0800

    feat: link from interpro show page to heatmap

--- components/tables/InterproShowTable.tsx
+++ components/tables/InterproShowTable.tsx
@@ -1,11 +1,17 @@
-import React from "react"
+import { useContext, useMemo } from "react"
 
 import LocalPaginatedTable from "./generics/LocalPaginatedTable"
 import TextLink from "../atomic/TextLink"
 import ShowMoreList from "../atomic/texts/ShowMoreList"
+import { useRouter } from "next/router"
+import { GlobalStoreContext } from "../../pages/_app"
 
 const InterproShowTable = ({ data }) => {
-  const columns = React.useMemo(
+
+  const router = useRouter();
+  const { setNavigationPayload } = useContext(GlobalStoreContext);
+
+  const columns = useMemo(
     () => [
       {
         Header: "Species",
@@ -22,15 +28,37 @@ const InterproShowTable = ({ data }) => {
         Cell: ({ value, row }) => (
           <ShowMoreList
             items={value.map((gene, i) => (
-              <TextLink href={`/species/${row.original.species.tax}/genes/${gene.label}`} key={i}>
+              <TextLink
+                href={`/species/${row.original.species.tax}/genes/${gene.label}`}
+                key={i}
+              >
                 {gene.label}
               </TextLink>
             ))}
           />
         ),
       },
-    ], []
-  )
+      {
+        Header: "Heatmap",
+        Cell: ({ value, row }) => (
+          <TextLink
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              const taxid = row.original.species.tax;
+              const geneLabels = row.values.genes.map((gene) => gene.label);
+              const payload = geneLabels.map((label) => ({ taxid, label }));
+              setNavigationPayload(payload);
+              router.push("/heatmap");
+            }}
+          >
+            Go to heatmap
+          </TextLink>
+        ),
+      },
+    ],
+    []
+  );
 
   return (
     <LocalPaginatedTable
